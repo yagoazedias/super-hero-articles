a94ee336283b804b1c83a48bb0dbe80bc3ba7a49
yagoazedias <yagoazedias@gmail.com>
2018-05-18 10:29:16 -0300

    Removed unused methods

--- common/router.ts
+++ common/router.ts
@@ -1,7 +1,6 @@
 import * as restify from 'restify'
 import {EventEmitter} from 'events'
 import {NotFoundError} from 'restify-errors'
-import * as js2xmlparser from "js2xmlparser";
 
 export abstract class Router extends EventEmitter {
     abstract applyRoutes(application: restify.Server)
@@ -24,7 +23,6 @@ export abstract class Router extends EventEmitter {
 
                 response.json(this.envelope(document));
             }else{
-                console.log(document);
                 throw new NotFoundError('Documento não encontrado')
             }
             return next()
@@ -46,33 +44,4 @@ export abstract class Router extends EventEmitter {
             next();
         }
     }
-
-    renderAllXml(response: restify.Response, next: restify.Next){
-        return (documents: any[])=>{
-            if(documents){
-                documents.forEach(document=>{
-                    this.emit('beforeRender', document)
-                });
-
-                response.setHeader("Content-Type", "application/xml");
-                response.send(js2xmlparser.parse("root", JSON.parse(JSON.stringify(documents))));
-            } else {
-                response.send(js2xmlparser.parse("root", JSON.parse(JSON.stringify([]))));
-            }
-        }
-    }
-
-    renderForCallback(document, response: restify.Response, next: restify.Next){
-        return (document) => {
-            if(document){
-                this.emit('beforeRender', document);
-                response.json(document)
-            }else{
-                console.log(document);
-                throw new NotFoundError('Documento não encontrado')
-            }
-            return next()
-        }
-    }
-
 }
